9e7c4bd4c37a620e1ae868f0b0926b96b34d942c
ignat <ignatik.klokov@gmail.com>
2025-03-16 22:08:49 +0200

    primitive turn sys

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <raylib.h>
 #include <stdio.h>
+#include <sys/types.h>
 
 const int SCREENX = 1920;
 const int SCREENY = 1080;
@@ -11,28 +12,32 @@ const int GAP = 2;
 const int GRIDX = 30;
 const int GRIDY = 20;
 
+const Color BACKGROUND = BLACK;
+const Color teams[] = {DARKGREEN, RED, PURPLE, YELLOW, DARKBLUE, {0xff, 0x7d, 0x31, 0xff}};
+
 Color getTeamColor(int i, int k) {
   if (k < GRIDY / 2) {
     if (i < GRIDX / 3) {
-      return DARKGREEN;
+      return teams[0];
     } else if (i >= GRIDX / 3 && i < GRIDX / 3 * 2) {
-      return RED;
+      return teams[1];
     }
-    return PURPLE;
+    return teams[2];
   } else {
     if (i < GRIDX / 3) {
-      return YELLOW;
+      return teams[3];
     } else if (i >= GRIDX / 3 && i < GRIDX / 3 * 2) {
-      return DARKBLUE;
+      return teams[4];
     }
-    return ORANGE;
+    return teams[5];
   }
 }
 
+int turn = 0;
 void draw() {
   BeginDrawing();
 
-  ClearBackground(BLACK);
+  ClearBackground(BACKGROUND);
 
   int rectSizeX = (SIZE + GAP) * GRIDX;
   int rectSizeY = (SIZE + GAP) * GRIDY;
@@ -40,22 +45,51 @@ void draw() {
   int startX = (SCREENX - rectSizeX) / 2;
   int startY = (SCREENY - rectSizeY) / 2;
 
-  // DrawRectangle(startX, startY, rectSizeX, rectSizeY, GREEN);
+  if (turn == 0) {
+    DrawRectangle(0, 0, startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  } else if (turn == 1) {
+    DrawRectangle(startX + (rectSizeX / 3), 0, (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  } else if (turn == 2) {
+    DrawRectangle(startX + (rectSizeX / 3 * 2), 0, startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  } else if (turn == 3) {
+    DrawRectangle(0, startY + (rectSizeY / 2), startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  } else if (turn == 4) {
+    DrawRectangle(startX + (rectSizeX / 3), startY + (rectSizeY / 2), (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  } else if (turn == 5) {
+    DrawRectangle(startX + (rectSizeX / 3 * 2), startY + (rectSizeY / 2), startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  }
+
+  DrawRectangle(startX, startY, rectSizeX, rectSizeY, BACKGROUND);
+
   for (int i = 0; i < GRIDX; i++) {
     for (int k = 0; k < GRIDY; k++) {
       DrawRectangle(startX + 1 + ((SIZE + GAP) * i),
                     startY + 1 + ((SIZE + GAP) * k),
                     SIZE, SIZE,
                     getTeamColor(i, k));
-      // DrawText(TextFormat("%d", k), startX + 1 + ((size + gap) * i), startY +
-      // 1 + ((size + gap) * k), 12, BLACK);
     }
   }
 
   EndDrawing();
 }
 
-void processInput() {}
+void processInput() {
+  if (IsKeyPressed(KEY_Q)) {
+    if (turn == 0) {
+      turn = 1;
+    } else if (turn == 1) {
+      turn = 2;
+    } else if (turn == 2) {
+      turn = 5;
+    } else if (turn == 5) {
+      turn = 4;
+    } else if (turn == 4) {
+      turn = 3;
+    } else if (turn == 3) {
+      turn = 0;
+    }
+  }
+}
 
 int main() {
   InitWindow(SCREENX, SCREENY, "window");
