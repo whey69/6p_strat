e5bf8eb99c19ccc2d3150fb701a56e915c438563
ignat <ignatik.klokov@gmail.com>
2025-03-16 21:40:20 +0200

    squares 🤑

--- main.c
+++ main.c
@@ -1,31 +1,66 @@
-#include <stdio.h>
+#include "raylib.h"
 #include <raylib.h>
+#include <stdio.h>
 
-const int screenWidth = 1920;
-const int screenHeight = 1080;
+const int SCREENX = 1920;
+const int SCREENY = 1080;
 
-Color textColor = LIGHTGRAY;
+// so that one square + gap around it = 32
+const int SIZE = 30;
+const int GAP = 2;
+const int GRIDX = 30;
+const int GRIDY = 20;
+
+Color getTeamColor(int i, int k) {
+  if (k < GRIDY / 2) {
+    if (i < GRIDX / 3) {
+      return DARKGREEN;
+    } else if (i >= GRIDX / 3 && i < GRIDX / 3 * 2) {
+      return RED;
+    }
+    return PURPLE;
+  } else {
+    if (i < GRIDX / 3) {
+      return YELLOW;
+    } else if (i >= GRIDX / 3 && i < GRIDX / 3 * 2) {
+      return DARKBLUE;
+    }
+    return ORANGE;
+  }
+}
+
 void draw() {
   BeginDrawing();
-  
-  ClearBackground(RAYWHITE);
-  DrawText("const char *text", screenWidth/2, screenHeight/2, 24, textColor);
+
+  ClearBackground(BLACK);
+
+  int rectSizeX = (SIZE + GAP) * GRIDX;
+  int rectSizeY = (SIZE + GAP) * GRIDY;
+
+  int startX = (SCREENX - rectSizeX) / 2;
+  int startY = (SCREENY - rectSizeY) / 2;
+
+  // DrawRectangle(startX, startY, rectSizeX, rectSizeY, GREEN);
+  for (int i = 0; i < GRIDX; i++) {
+    for (int k = 0; k < GRIDY; k++) {
+      DrawRectangle(startX + 1 + ((SIZE + GAP) * i),
+                    startY + 1 + ((SIZE + GAP) * k),
+                    SIZE, SIZE,
+                    getTeamColor(i, k));
+      // DrawText(TextFormat("%d", k), startX + 1 + ((size + gap) * i), startY +
+      // 1 + ((size + gap) * k), 12, BLACK);
+    }
+  }
 
   EndDrawing();
 }
 
-void processInput() {
-  if (IsMouseButtonDown(0)) {
-    textColor = RED;
-  } else {
-    textColor = LIGHTGRAY;
-  }
-}
+void processInput() {}
 
 int main() {
-  InitWindow(screenWidth, screenHeight, "window");
+  InitWindow(SCREENX, SCREENY, "window");
   ToggleFullscreen();
-  
+
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
