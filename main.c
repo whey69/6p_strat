1aad62cf956a5dd3562c83b1704bcdd528561820
ignat <ignatik.klokov@gmail.com>
2025-03-16 20:51:43 +0200

    init

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <raylib.h>
+
+const int screenWidth = 1920;
+const int screenHeight = 1080;
+
+Color textColor = LIGHTGRAY;
+void draw() {
+  BeginDrawing();
+  
+  ClearBackground(RAYWHITE);
+  DrawText("const char *text", screenWidth/2, screenHeight/2, 24, textColor);
+
+  EndDrawing();
+}
+
+void processInput() {
+  if (IsMouseButtonDown(0)) {
+    textColor = RED;
+  } else {
+    textColor = LIGHTGRAY;
+  }
+}
+
+int main() {
+  InitWindow(screenWidth, screenHeight, "window");
+  ToggleFullscreen();
+  
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    processInput();
+    draw();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
