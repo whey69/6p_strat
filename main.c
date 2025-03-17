5613ec604c70a604d1ab8b5e22449c40b231704b
ignat <ignatik.klokov@gmail.com>
2025-03-17 09:09:09 +0200

    4p + bullet impl 1

--- main.c
+++ main.c
@@ -4,38 +4,83 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-const int SCREENX = 1920;
-const int SCREENY = 1080;
+#define SCREENX 1920
+#define SCREENY 1080
 
 // so that one square + gap around it = 32
-const int SIZE = 30;
-const int GAP = 2;
-const int GRIDX = 30;
-const int GRIDY = 20;
+#define SIZE 30
+#define GAP 2
+#define GRIDX 20
+#define GRIDY 20
 
 const Color BACKGROUND = BLACK;
 const Color teams[] = {DARKGREEN, RED, PURPLE, YELLOW, DARKBLUE, {0xff, 0x7d, 0x31, 0xff}};
 
+typedef struct {
+  int pos[2];
+  int teams;
+} Cell;
+Cell field[GRIDX][GRIDY];
+
+typedef struct {
+  int pos[2];
+  int vel[2];
+  int team;
+} Bullet;
+
+// max 1024 per player !important
+// + 4 just in case
+Bullet onFieldBullets[4100];
+
+// these guys are for visual storage
+Bullet greenBullets[1024]; 
+Bullet redBullets[1024];
+Bullet yellowBullets[1024];
+Bullet blueBullets[1024];
+
+enum Teams {
+  _GREEN,
+  _RED,
+  _PURPLE,
+  _YELLOW,
+  _DARKBLUE,
+  _DARKORANGE
+};
+
 Color getTeamColor(int i, int k) {
   if (k < GRIDY / 2) {
-    if (i < GRIDX / 3) {
+    if (i < GRIDX / 2) {
       return teams[0];
-    } else if (i >= GRIDX / 3 && i < GRIDX / 3 * 2) {
+    } else {
       return teams[1];
     }
-    return teams[2];
+    // return teams[2];
   } else {
-    if (i < GRIDX / 3) {
+    if (i < GRIDX / 2) {
       return teams[3];
-    } else if (i >= GRIDX / 3 && i < GRIDX / 3 * 2) {
+    } else {
       return teams[4];
     }
-    return teams[5];
+    // return teams[5];
   }
 }
 
 int turn = 0;
-void draw() {
+void processFrame() {
+  /// input handling
+  if (IsKeyPressed(KEY_Q)) {
+    if (turn == _GREEN) {
+      turn = _RED;
+    } else if (turn == _RED) {
+      turn = _DARKBLUE;
+    } else if (turn == _DARKBLUE) {
+      turn = _YELLOW;
+    } else {
+      turn = 0;
+    }
+  }
+
+  /// drawing
   BeginDrawing();
 
   ClearBackground(BACKGROUND);
@@ -46,18 +91,18 @@ void draw() {
   int startX = (SCREENX - rectSizeX) / 2;
   int startY = (SCREENY - rectSizeY) / 2;
 
-  if (turn == 0) {
-    DrawRectangle(0, 0, startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
-  } else if (turn == 1) {
-    DrawRectangle(startX + (rectSizeX / 3), 0, (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
-  } else if (turn == 2) {
-    DrawRectangle(startX + (rectSizeX / 3 * 2), 0, startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
-  } else if (turn == 3) {
-    DrawRectangle(0, startY + (rectSizeY / 2), startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
-  } else if (turn == 4) {
-    DrawRectangle(startX + (rectSizeX / 3), startY + (rectSizeY / 2), (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
-  } else if (turn == 5) {
-    DrawRectangle(startX + (rectSizeX / 3 * 2), startY + (rectSizeY / 2), startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  if (turn == _GREEN) {
+    DrawRectangle(0, 0, SCREENX / 2, SCREENY / 2, teams[turn]);
+  } else if (turn == _RED) {
+    DrawRectangle(SCREENX / 2, 0, SCREENX / 2, SCREENY / 2, teams[turn]);
+  // } else if (turn == 2) {
+  //   DrawRectangle(startX + (rectSizeX / 3 * 2), 0, startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
+  } else if (turn == _YELLOW) {
+    DrawRectangle(0, SCREENY / 2, SCREENX / 2, SCREENY / 2, teams[turn]);
+  } else if (turn == _DARKBLUE) {
+    DrawRectangle(SCREENX / 2, SCREENY / 2, SCREENX / 2, SCREENY / 2, teams[turn]);
+  // } else if (turn == 5) {
+    // DrawRectangle(startX + (rectSizeX / 3 * 2), startY + (rectSizeY / 2), startX + (rectSizeX / 3), startY + (rectSizeY / 2), teams[turn]);
   }
 
   DrawRectangle(startX, startY, rectSizeX, rectSizeY, BACKGROUND);
@@ -71,41 +116,7 @@ void draw() {
     }
   }
 
-  if (GuiButton((Rectangle){100, 100, 120, 30}, "hello raygui")) {
-    if (turn == 0) {
-      turn = 1;
-    } else if (turn == 1) {
-      turn = 2;
-    } else if (turn == 2) {
-      turn = 5;
-    } else if (turn == 5) {
-      turn = 4;
-    } else if (turn == 4) {
-      turn = 3;
-    } else if (turn == 3) {
-      turn = 0;
-    }
-  }
-
   EndDrawing();
-}
-
-void processInput() {
-  if (IsKeyPressed(KEY_Q)) {
-    if (turn == 0) {
-      turn = 1;
-    } else if (turn == 1) {
-      turn = 2;
-    } else if (turn == 2) {
-      turn = 5;
-    } else if (turn == 5) {
-      turn = 4;
-    } else if (turn == 4) {
-      turn = 3;
-    } else if (turn == 3) {
-      turn = 0;
-    }
-  }
 }
 
 int main() {
@@ -115,8 +126,7 @@ int main() {
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-    processInput();
-    draw();
+    processFrame();
   }
 
   CloseWindow();
